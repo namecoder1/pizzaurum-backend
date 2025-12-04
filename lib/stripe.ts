a8e8cc503d3eb23184cc2efafb700a7b1c662c4c
namecoder1 <bartolomei.private@gmail.com>
2025-12-04 13:25:18 +0100

    env vars runtime fix

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -4,11 +4,12 @@ import dotenv from "dotenv";
 dotenv.config();
 let sKey: string;
 
-if (process.env.NODE_ENV === 'production') {
-  sKey = process.env.STRIPE_SECRET_KEY!;
-} else {
-  sKey = process.env.STRIPE_SECRET_KEY_TEST!;
-}
+const appEnv = process.env.APP_ENV || process.env.NODE_ENV;
+const isProdLike = appEnv === 'production' || appEnv === 'preview';
+
+sKey = isProdLike
+  ? process.env.STRIPE_SECRET_KEY!
+  : process.env.STRIPE_SECRET_KEY_TEST!;
 
 export const stripe = new Stripe(sKey, {
   apiVersion: '2025-11-17.clover'
